a5db43968e625e4aa3a92369ba53c68a09ba2225
manwoodtWindows <manwoodt@gmail.com>
2024-11-28 15:25:39 +0300

    clean build.gradle.kts

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -56,29 +57,25 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":data"))
 
-    implementation (libs.androidx.lifecycle.runtime.compose) // Для collectAsStateWithLifecycle
-    implementation (libs.androidx.lifecycle.livedata.ktx)
-    implementation (libs.androidx.lifecycle.runtime.ktx.v260) // Для ViewModel и жизненного цикла
-    implementation (libs.dagger)
+
     implementation(libs.hilt)
     kapt(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt)
+
+    implementation(libs.androidx.runtime.livedata)
+
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.runtime.ktx)
-    implementation(libs.androidx.runtime.livedata)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
+
+
 }
--- src/data/build.gradle.kts
+++ src/data/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     kotlin("plugin.serialization") version "2.0.0"
-    alias(libs.plugins.dagger.hilt)
     id("kotlin-kapt")
 }
 
@@ -36,20 +35,13 @@ android {
 }
 
 dependencies {
-    implementation(libs.hilt)
-    kapt(libs.hilt.android.compiler)
-    implementation(libs.androidx.hilt)
-
     implementation(project(":domain"))
+
     implementation (libs.dagger)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.hilt)
+
+    implementation (libs.retrofit)
 
     implementation(libs.kotlinx.serialization.json)
-    implementation (libs.retrofit)
     implementation (libs.converter.gson)
 }
